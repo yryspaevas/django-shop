1fd2c928e276a455167dd8bf6da489d80f732a7d
eles <yryspaeva22@gmail.com>
2022-12-07 13:00:01 +0600

    admin

--- main/models.py
+++ main/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -12,3 +15,6 @@ class Product(models.Model):
     quantity = models.IntegerField()
     description = models.TextField()
     status = models.CharField(max_length=15, choices=[('есть в наличии', 'in stock'), ('нет в наличии', 'out of stock'), ('ожидается', 'pending')])
+
+    def __str__(self):
+        return f'[{self.category}] -> {self.title}'
