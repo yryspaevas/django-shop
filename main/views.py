8448bea9c842023bb52f110985df4fc1808e94c6
eles <yryspaeva22@gmail.com>
2022-12-13 11:04:21 +0600

    swagger

--- main/views.py
+++ main/views.py
@@ -18,6 +18,9 @@ from .filters import ProductFilter
 # для поиска по нескольким полям
 from django.db.models import Q
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 
 
 class CategoryViewSet(ModelViewSet):
@@ -37,6 +40,10 @@ class ProductViewSet(ModelViewSet):
             return [] # то разрешаем всем
         return [IsAdminUser()]
 
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('q', openapi.IN_QUERY, type=openapi.TYPE_STRING),
+    ])
     @action(['GET'], detail=False)
     def search(self, request):
         # /products/search/?q=hello
--- review/views.py
+++ review/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from drf_yasg.utils import swagger_auto_schema
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsAuthorOrReadOnly
 
@@ -24,6 +25,7 @@ class ComentViewSet(ModelViewSet):
 class CreateRatingAPIView(APIView):
     permission_classes  = [IsAuthenticated]
 
+    @swagger_auto_schema(request_body=RatingSerializer())
     def post(self, request):
         user = request.user
         ser = RatingSerializer(data=request.data, context={"request":request})
