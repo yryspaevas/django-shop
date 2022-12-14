9ff7cf953ccfbd459a714cb4e86fac558a4f8c25
eles <yryspaeva22@gmail.com>
2022-12-14 10:36:35 +0600

    images

--- config/urls.py
+++ config/urls.py
@@ -41,3 +41,10 @@ urlpatterns = [
     path('', include('review.urls')),
     
 ]
+
+""" подключение медиа и статик"""
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/models.py
+++ main/models.py
@@ -16,6 +16,8 @@ class Product(models.Model):
     description = models.TextField()
     status = models.CharField(max_length=15, choices=[('есть в наличии', 'in stock'), ('нет в наличии', 'out of stock'), ('ожидается', 'pending')])
 
+    image = models.ImageField(upload_to='products', null=True)
+
     def __str__(self):
         return f'[{self.category}] -> {self.title}'
 
