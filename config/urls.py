7098b2b1fde302f2282ac9e8ff8c778d09467832
eles <yryspaeva22@gmail.com>
2022-12-27 11:39:49 +0600

    deploy settings

--- config/urls.py
+++ config/urls.py
@@ -36,7 +36,7 @@ swagger_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', swagger_view.with_ui('swagger', cache_timeout=0)),
-    path('account/', include('account.urls')),
+    path('', include('account.urls')),
     path('', include('main.urls')),
     path('', include('review.urls')),
     
